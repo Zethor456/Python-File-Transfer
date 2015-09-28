c65801b91be43d6f7671a919a8407fa081f5322e
Filip Todoric <filiptodoric@gmail.com>
2015-09-28 17:41:36 -0400

    cd is now working

--- client.py
+++ client.py
@@ -2,6 +2,10 @@ import socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect(('localhost',4040))
 
-cmd = raw_input("Enter the command you want to send: ")
-if cmd == "ls":
-    socket.send(cmd);
+input = raw_input("Enter the command you want to send: ")
+cmd = input.split()
+if cmd[0] == "ls":
+    socket.send(cmd[0])
+if cmd[0] == "cd":
+    socket.send(cmd[0])
+    socket.send(cmd[1])
--- server.py
+++ server.py
@@ -2,11 +2,16 @@ import socket
 import os
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('localhost',4040))
-
 serversocket.listen(5)
+
+currDirectory = os.getcwd()
 while True:
 	c, addr = serversocket.accept()
 	print 'Connected to ', addr
 	msg = c.recv(4096)
+	print msg
 	if msg == "ls":
-		print os.listdir(os.getcwd())
+		print os.listdir(currDirectory)
+	if msg == "cd":
+		currDirectory = c.recv(4096)
+		print os.listdir(currDirectory)
