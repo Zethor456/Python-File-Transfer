48ffe48848bc2b294d61ea5af2f6b1af171a4f5b
Zethor456 <axdfgnj@gmail.com>
2015-09-28 19:54:48 -0400

    get and put sort of working
    I was having issues with the buffer of the original msg = c.recv() combining the two sends for the command and filename

--- server.py
+++ server.py
@@ -18,3 +18,30 @@ while True:
 		toAdd = c.recv(4096)
 		newDir = currDirectory + "/" + toAdd
 		os.mkdir(newDir)
+	if msg == "get":
+		filenme = c.recv(1024)
+		sentfile = open("Test.txt","r");
+		chunk = sentfile.read(4096)
+		while(chunk):
+			print("sent stuff")
+			c.send(chunk)
+			
+			chunk = sentfile.read(4096)
+		sentfile.close	
+		c.shutdown(socket.SHUT_WR)
+		print c.recv(4096)
+		c.close
+	if msg == "put":
+		print("got here")
+		filenme = c.recv(4096)
+		print("got here")
+		f = open(filenme,"wb");
+		print("file ok")
+		chunk = c.recv(4096)
+		while(chunk):
+			print("got stuff")
+			f.write(chunk)
+			chunk = c.recv(4096)
+		f.close()
+		c.recv(1024);
+		c.close	
