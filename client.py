72e4c24799afc556a09ca2c0372105ceac994dde
Filip Todoric <filiptodoric@gmail.com>
2015-09-28 16:57:46 -0400

    basic client/server and ls function

--- client.py
+++ client.py
@@ -2,11 +2,6 @@ import socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect(('localhost',4040))
 
-#the binary string initialized via b"" is due to
-#some kind of compatibility issue between python 2 & 3
-PING = b"ping"
-
-socket.send(PING)
-response = socket.recv(512)
-print response
-socket.close()
+cmd = raw_input("Enter the command you want to send: ")
+if cmd == "ls":
+    socket.send(cmd);
