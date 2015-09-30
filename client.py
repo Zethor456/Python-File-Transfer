4e2a733bae0c8ee28b2ed110e13915ff53d6399d
Eric <eric.nobody@gmail.com>
2015-09-30 12:55:54 -0400

    Numerous bug fixes.

--- client.py
+++ client.py
@@ -2,46 +2,48 @@ import socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect(('localhost',4040))
 
-while True:
-    input = raw_input("Enter the command you want to send: ")
-    cmd = input.split()
+running = True
+while running:
+    raw_cmd = raw_input("Enter the command you want to send: ")
+    cmd = raw_cmd.split()
     if cmd[0] == "ls":
         socket.send(cmd[0])
         listElements = socket.recv(4096)
         print listElements
-    if cmd[0] == "cd":
+    elif cmd[0] == "cd":
+        socket.send(raw_cmd)
+    elif cmd[0] == "mkdir":
+        socket.send(raw_cmd)
+    elif cmd[0] == "get":
         socket.send(cmd[0])
+        print(cmd[0])
         socket.send(cmd[1])
-        listElements = socket.recv(4096)
-        print listElements
-    if cmd[0] == "mkdir":
+        print(cmd[1])
+        f = open("other.txt","wb")
+        chunk = socket.recv(4096)
+        while(chunk):
+            print("got stuff")
+            print(chunk)
+            f.write(chunk)
+            chunk = socket.recv(4096)
+        f.close()
+        socket.recv(4096);
+        #socket.close
+    elif cmd[0] == "put":
         socket.send(cmd[0])
-        socket.send(cmd[1])
-    if cmd[0] == "get":
-    	socket.send(cmd[0])
-    	print(cmd[0])
-    	socket.send(cmd[1])
-    	print(cmd[1])
-    	f = open("other.txt","wb")
-    	chunk = socket.recv(4096)
-    	while(chunk):
-    		print("got stuff")
-    		print(chunk)
-    		f.write(chunk)
-    		chunk = socket.recv(4096)
-    	f.close()
-    	socket.recv(4096);
-    	socket.close
-    if cmd[0] == "put":
-    	socket.send(cmd[0])
-    	socket.send(cmd[1])
-    	f = open("Test.txt","r")
-    	chunk = f.read(4096)
-    	while(chunk):
-    		socket.send(chunk)
-    		print("sent stuff")
-    		chunk = f.read(4096)
-    	f.close
-    	#socket.shutdown(socket.SHUT_WR)
-    	print socket.recv(4096)
-    	socket.close
+        f = open("Test.txt","r")
+        chunk = f.read(4096)
+        while(chunk):
+            socket.send(chunk)
+            print("sent stuff")
+            chunk = f.read(4096)
+        f.close
+        #socket.shutdown(socket.SHUT_WR)
+        print socket.recv(4096)
+        #socket.close
+    elif cmd[0] == "exit":
+        socket.send("terminate")
+        socket.close()
+        running = False
+    else:
+        print ("Unkown command: "+cmd[0])
--- server.py
+++ server.py
@@ -5,50 +5,53 @@ serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serversocket.bind(('localhost',4040))
 serversocket.listen(5)
 
-currDirectory = os.getcwd()
+
 while True:
 	c, addr = serversocket.accept()
 	print 'Connected to ', addr
-	msg = c.recv(4096)
-	if msg == "ls":
-		lsDir = os.listdir(currDirectory)
-		string = '\n'.join(lsDir)
-		c.send(string)
-		c.close()
-	if msg == "cd":
-		currDirectory = c.recv(4096)
-		lsDir = os.listdir(currDirectory)
-		string = '\n'.join(lsDir)
-		c.send(string)
-		c.close()
-	if msg == "mkdir":
-		toAdd = c.recv(4096)
-		newDir = currDirectory + "/" + toAdd
-		os.mkdir(newDir)
-	if msg == "get":
-		filenme = c.recv(1024)
-		sentfile = open("Test.txt","r");
-		chunk = sentfile.read(4096)
-		while(chunk):
-			print("sent stuff")
-			c.send(chunk)
-
-			chunk = sentfile.read(4096)
-		sentfile.close
-		c.shutdown(socket.SHUT_WR)
-		print c.recv(4096)
-		c.close
-	if msg == "put":
-		print("got here")
-		filenme = c.recv(4096)
-		print("got here")
-		f = open(filenme,"wb");
-		print("file ok")
-		chunk = c.recv(4096)
-		while(chunk):
-			print("got stuff")
-			f.write(chunk)
+	running = True
+	while running:
+		raw_msg = c.recv(4096)
+		msg = raw_msg.split()
+		cmd = msg[0]
+		print 'Received cmd ', cmd
+		if cmd == "ls":
+			lsDir = os.listdir(os.getcwd())
+			#Check for empty directory
+			if not lsDir: string = "*empty*"
+			else:
+				string = '\n'.join(lsDir)
+				print "string: ",string
+			c.send(string)
+		elif cmd == "cd":
+			target = msg[1]
+			os.chdir(target)
+			print os.getcwd()
+		elif cmd == "mkdir":
+			target = msg[1]
+			newDir = os.getcwd() + "/" + target
+			os.mkdir(newDir)
+		elif cmd == "get":
+			target = msg[1]
+			f = open(target,"r");
+			chunk = f.read(4096)
+			while(chunk):
+				print("sending stuff")
+				c.send(chunk)
+				chunk = f.read(4096)
+			f.close
+		elif cmd == "put":
+			target = msg[1]
+			f = open(target,"wb");
+			print("file ok")
 			chunk = c.recv(4096)
-		f.close()
-		c.recv(1024);
-		c.close
+			while(chunk):
+				print("got stuff")
+				f.write(chunk)
+				chunk = c.recv(4096)
+			f.close()
+		elif cmd == "terminate":
+			c.close()
+			running = False
+		else:
+			print("Received unkown cmd: "+cmd)
